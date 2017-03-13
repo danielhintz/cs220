a34cbfcfd2a680cc2baed55172e8727e99d0d2a0
Daniel Hintz <dhintz1@cs.binghamton.edu>
2017-03-13 18:54:42 -0400

    stuffs

--- projects/proj2/floatx.c
+++ projects/proj2/floatx.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//#define DEBUG
-
+#define DEBUG
 typedef char bool;
 
 floatx getBits(floatx num, floatx start, floatx end);
@@ -22,41 +21,57 @@ floatx doubleToFloatx(const floatxDef *def, double value)
   floatx dBits = *((floatx*) &value);
   int fBits = def->totBits-def->expBits-1;
   floatx result = 0;
-
-  // bool denormal;
+  
+  bool denormal = 0;
 
   if(value<0)result = (floatx)1 << (def->totBits-1);
   
   floatx exponent = getBits(dBits, 62, 52);
   floatx frac = getBits(dBits, 51, 0) >> (51-fBits);
-  bool carry = frac & 1;
-  frac >>= 1;
+  bool carry = 0;
 
   if(exponent == 0 && frac == 0) return 0;
-  if(exponent == 0)
+
+  if(exponent <= 895)
     {
-      //Denormalized number
-      //This isn't run for 1.27563e+45
+      denormal = 1;
     }
-  
   exponent -= 1023;
   exponent += (int)pow(2, def->expBits-1)-1;
   
+  if(!denormal && exponent > (int)pow(2, def->expBits)-1)
+    {
+      exponent = (int)pow(2,def->expBits)-1;
+      frac = 0;
+      carry = 0;
+    }
+  if(exponent == 0 || denormal)
+    {
+      denormal = 1;
+      exponent = 0;
+      frac = getBits(dBits, 51, 0) >> (51-fBits+2);
+    }
+  carry = frac & 1;
+  if(!denormal)
+  frac >>=1;  
   exponent <<= (fBits);
 
+  if(denormal) frac |= ((floatx)1<<(fBits-1));
   if(carry)frac++;
   result |= (exponent) | (frac);
-
 #ifdef DEBUG
   print_bits(dBits);
   
-  print_bits(exponent);
+  //  print_bits(exponent);
   
-  print_bits(frac);
+  // print_bits(frac);
   
   print_bits(result);
 
-  printf("%lx  %lx  %lx\n", dBits, exponent, frac);
+  float x = (float)value;
+  print_bits(*(unsigned int*)(&x));
+
+  printf("%lx  %lx  %lx\n", dBits, exponent, (frac));
 #endif
   return result;
 }
